731fdbe640ce0d3c52eed068893fd537bd178128
Randall Sewell-Edmonds <randallsewell@gmail.com>
2015-09-22 10:06:46 -0700

    Minor update

--- cachematrix.R
+++ cachematrix.R
@@ -39,6 +39,8 @@ cacheSolve <- function(x, ...) {
     cacheMatrix
 }
 
+
+## Checks equality of matrices. 
 matequal <- function(x, y) {
     is.matrix(x) && is.matrix(y) && dim(x) == dim(y) && all(x == y)
 }
